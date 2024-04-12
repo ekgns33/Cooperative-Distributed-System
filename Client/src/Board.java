d27f6a799e9cb29651d303d41f2bf5879b3c76b9
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2024-04-12 17:20:01 +0900

    Feat: add button actionlistener

--- Client/src/Board.java
+++ Client/src/Board.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Board extends JFrame {
+    int curButtonIdx;
+    JButton[] button;
 
     public Board() {
         setTitle("Shared Whiteboard");
@@ -19,7 +21,7 @@ public class Board extends JFrame {
         JPanel figureTypePanel = new JPanel(new GridLayout(2, 2));
         JPanel figureModifyPanel = new JPanel(new GridLayout(2, 2));
 
-        JButton[] button = new JButton[8];
+        button = new JButton[8];
 
         button[0] = new JButton("원");
         button[1] = new JButton("사각형");
@@ -30,19 +32,43 @@ public class Board extends JFrame {
         button[6] = new JButton("선 색상");
         button[7] = new JButton("채우기 색상");
 
-        for (int i = 0; i < 4; i++ )
-            figureTypePanel.add(button[i]);
-        for (int i = 4; i < 8; i++ )
-            figureModifyPanel.add(button[i]);
+        ButtonListener buttonListener = new ButtonListener();
+        for (int i = 0; i < button.length; i++) {
+            button[i].addActionListener(buttonListener);
+            button[i].setFocusPainted(false);
+            if (i < 4) {
+                figureTypePanel.add(button[i]);
+            } else {
+                figureModifyPanel.add(button[i]);
+            }
+        }
+
+        curButtonIdx = 0;
+        button[curButtonIdx].setEnabled(false);
+
         buttonPanel.add(figureTypePanel);
         buttonPanel.add(figureModifyPanel);
 
         add(buttonPanel, BorderLayout.NORTH);
     }
 
-    private void boardInit(){
+    private void boardInit() {
         JPanel drawingPanel = new JPanel();
         drawingPanel.setBackground(Color.WHITE);
         add(drawingPanel, BorderLayout.CENTER);
+    }
+
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            button[curButtonIdx].setEnabled(true);
+            for (int i = 0; i < 8; i++) {
+                if ((JButton) e.getSource() == button[i]) {
+                    button[i].setEnabled(false);
+                    curButtonIdx = i;
+                }
+            }
+
+        }
     }
 }
