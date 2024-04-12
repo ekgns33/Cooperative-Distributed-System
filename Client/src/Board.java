fa1c139680fdfc2dd9d0ac97837723f1527e2150
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2024-04-12 16:55:49 +0900

    Feat 버튼 레이아웃 설정

--- Client/src/Board.java
+++ Client/src/Board.java
@@ -16,16 +16,27 @@ public class Board extends JFrame {
     }
 
     private void buttonInit() {
-        buttonPanel = new JPanel(new GridLayout(2, 2));
-        JButton[] buttons = new JButton[4];
+        buttonPanel = new JPanel(new GridLayout(1, 2));
+        JPanel figureTypePanel = new JPanel(new GridLayout(2, 2));
+        JPanel figureModifyPanel = new JPanel(new GridLayout(2, 2));
 
-        buttons[0] = new JButton("button 0");
-        buttons[1] = new JButton("button 1");
-        buttons[2] = new JButton("button 2");
-        buttons[3] = new JButton("button 3");
+        JButton[] button = new JButton[8];
 
-        for (JButton button : buttons)
-            buttonPanel.add(button);
+        button[0] = new JButton("원");
+        button[1] = new JButton("사각형");
+        button[2] = new JButton("선");
+        button[3] = new JButton("텍스트");
+        button[4] = new JButton("선 굵기");
+        button[5] = new JButton("색 채우기");
+        button[6] = new JButton("선 색상");
+        button[7] = new JButton("채우기 색상");
+
+        for (int i = 0; i < 4; i++ )
+            figureTypePanel.add(button[i]);
+        for (int i = 4; i < 8; i++ )
+            figureModifyPanel.add(button[i]);
+        buttonPanel.add(figureTypePanel);
+        buttonPanel.add(figureModifyPanel);
 
         add(buttonPanel, BorderLayout.NORTH);
     }
