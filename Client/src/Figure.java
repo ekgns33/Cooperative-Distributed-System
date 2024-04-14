048a3daecb2fe006dbaf4fe8f950e9d229312e1e
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2024-04-14 21:44:17 +0900

    Refactor: Define Figure interface

--- Client/src/Figure.java
+++ Client/src/Figure.java
@@ -1,14 +1,6 @@
 import java.awt.*;
-import java.math.BigInteger;
 
-public class Figure {
-    enum FigureType {CIRCLE, FILLED_CIRCLE, RECTANGLE, FILLED_RECTANGLE, LINE, PENCIL, TEXT}
-    FigureType type;
-    boolean fillColor;
-    Color drawColor;
-    BigInteger id;
-    float x, y, width, height, time, x2, y2;
-    String text;
-    int size;
-
+public interface Figure {
+    void draw(Graphics g);
+    boolean contains(Point p);
 }
