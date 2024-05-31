6db5d3d05c2228b2d8452b0ad031c50e64a2b0fb
ekgns33 <ekgns00421@gmail.com>
2024-05-31 15:05:00 +0900

    feat: 화이트보드 초기화를 위한 상태변수 및 로직 추가

--- Server/src/main/java/cooperativedistributedsystem/whiteboard/StompController.java
+++ Server/src/main/java/cooperativedistributedsystem/whiteboard/StompController.java
@@ -1,5 +1,7 @@
 package cooperativedistributedsystem.whiteboard;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.MessageHeaders;
@@ -19,6 +21,8 @@ public class StompController {
     private final SimpMessageSendingOperations sendingOperations;
     private final LockService lockService;
     private final String destination = "/room";
+    private AtomicBoolean loadLock = new AtomicBoolean(false);
+    private String loadUserSessionId = "";
 
     @MessageMapping("/pub")
     public void sendMessage(Message message, @Header("simpSessionId") String sessionId) {
@@ -32,20 +36,57 @@ public class StompController {
         }
         else if (message.getStatus() == 3) {
             log.info("message={}", message);
+            if(loadLock.get() && !sessionId.equals(loadUserSessionId)) {
+                return;
+            }
             messageRepository.save(message);
             sendingOperations.convertAndSend(destination, message);
         }
         else if (message.getStatus() == 4) {
             // send to '/room-user{sessionId}'
             // ex) /room-user13r1sadf13fa
+            if(loadLock.get()) {
+                return;
+            }
             log.info("lock!!. message={}, sessionId={}", message, sessionId);
             message.lockResult = lockService.lock(message.getId());
             MessageHeaders headers = createHeaders(sessionId);
             sendingOperations.convertAndSendToUser(sessionId, destination, message, headers);
         }
         else if (message.getStatus() == 5) {
+            if(loadLock.get()) {
+                return;
+            }
             log.info("unlock!!. message={}", message);
             lockService.unlock(message.getId());
+        }
+        else if (message.getStatus() == 6) {
+            if(!loadLock.compareAndSet(false, true)) {
+                message.lockResult = false;
+                MessageHeaders headers = createHeaders(sessionId);
+                sendingOperations.convertAndSendToUser(sessionId, destination, message, headers);
+                return;
+            }
+
+            log.info("clear page!!. message = {}", message);
+            loadUserSessionId = sessionId;
+
+            MessageHeaders headers = createHeaders(sessionId);
+            message.lockResult = true;
+            sendingOperations.convertAndSendToUser(sessionId, destination, message, headers);
+
+            message.lockResult = false;
+            message.status = 8;
+            sendingOperations.convertAndSend(destination, message);
+
+            messageRepository.deleteAll();
+            lockService.removeAll();
+        }
+        else if (message.getStatus() == 7){
+            log.info("client ready!!. message ={}", message);
+            loadLock.set(false);
+            loadUserSessionId = "";
+            sendingOperations.convertAndSend(destination, message);
         }
     }
 
